55d9f932c87561b6f1fd409ab9c92151e17485ba
Rufat Aliyev <rufat845@gmail.com>
2023-09-20 23:10:55 +0400

    init 2

--- express-01/app.js
+++ express-01/app.js
@@ -1,8 +1,20 @@
-const http = require('http');
+const http = require("http");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware!");
+  next();
+});
 
-app.listen(3002)
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log(`Server listening on ${process.env.PORT || 3002}`);
+})
